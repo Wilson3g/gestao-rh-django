d3aad79fde1535b33dedc26406463f9e793e28f9
wilson <wilson.sfj@outlook.com>
2020-08-07 22:13:52 -0300

    criando empresas

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('app.core.urls')),
     path('funcionarios/', include('app.funcionarios.urls')),
+    path('empresas/', include('app.empresas.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
