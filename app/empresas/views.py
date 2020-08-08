53530c704b03157f3d9c8d7841fc8eeb4a93584e
wilson <wilson.sfj@outlook.com>
2020-08-07 22:19:49 -0300

    adicionando form valid

--- app/empresas/views.py
+++ app/empresas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 from .models import Empresa
 
@@ -6,3 +7,11 @@ from .models import Empresa
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome']
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+
+        return HttpResponse('ok')
