ce31df0c335d7174c064a17c4311d68a1558b662
wilson <wilson.sfj@outlook.com>
2020-08-10 10:44:08 -0300

    listando e filtrando usuarios

--- app/funcionarios/views.py
+++ app/funcionarios/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Funcionario
 
-def home(request):
-    return HttpResponse('ola')
+class FuncionariosList(ListView):
+    model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresas
+        queryset = Funcionario.objects.filter(empresas=empresa_logada)
+        return queryset
