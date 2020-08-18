e91265e4c15141dae4f03fe4747b96948df78150
wilson <wilson.sfj@outlook.com>
2020-08-18 11:35:46 -0300

    comentando staticfiles

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -87,16 +87,15 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
